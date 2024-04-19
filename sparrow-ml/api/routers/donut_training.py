21f083a4ffa25ad522836a2dc0864b1270c7723c
guirateya <164197324+guirateya@users.noreply.github.com>
2024-04-19 14:14:11 +0200

    Update donut_training.py

--- sparrow-ml/api/routers/donut_training.py
+++ sparrow-ml/api/routers/donut_training.py
@@ -293,7 +293,7 @@ class DonutModelPLModule(pl.LightningModule):
 
         return scores
 
-    def validation_epoch_end(self, validation_step_outputs):
+    def on_validation_epoch_end(self, validation_step_outputs):
         # I set this to 1 manually
         # (previously set to len(self.config.dataset_name_or_paths))
         num_of_loaders = 1
